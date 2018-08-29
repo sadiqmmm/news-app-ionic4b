a239f5316fe2c414dd89b01025975117f192e673
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-08-30 03:30:07 +0530

    Add const variables and add http module

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/http';
+
+const API_URL = environment.apiUrl;
+const API_KEY = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(url) {
+     
+  }
 }

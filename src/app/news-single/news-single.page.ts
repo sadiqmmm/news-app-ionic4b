36ed1d464c7eb9cf7c2261ee415118b6199af2f1
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-08-30 04:06:38 +0530

    Add single page news on title click

--- src/app/news-single/news-single.page.ts
+++ src/app/news-single/news-single.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-news-single',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsSinglePage implements OnInit {
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    console.log(this.newsService.currentArticle);
   }
 
 }
--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -9,10 +9,11 @@ const API_KEY = environment.apiKey;
   providedIn: 'root'
 })
 export class NewsService {
+  currentArticle: any;
 
   constructor(private http: HttpClient) { }
 
   getData(url) {
-     return this.http.get(`${API_URL}/${url}&apiKey=${API_KEY}`);
+    return this.http.get(`${API_URL}/${url}&apiKey=${API_KEY}`);
   }
 }

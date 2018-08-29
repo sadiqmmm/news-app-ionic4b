17e51bf9f6185d61d1a31d00c768115654b52ab9
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-08-30 02:44:17 +0530

    Add the news page to default first page

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -5,6 +5,7 @@ import { TabsPage } from './tabs.page';
 import { HomePage } from '../home/home.page';
 import { AboutPage } from '../about/about.page';
 import { ContactPage } from '../contact/contact.page';
+import { NewsPage } from '../news/news.page';
 
 const routes: Routes = [
   {
@@ -12,14 +13,9 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirectTo: '/tabs/(home:home)',
-        pathMatch: 'full',
-      },
-      {
-        path: 'home',
-        outlet: 'home',
-        component: HomePage
+        path: 'news',
+        outlet: 'news',
+        component: NewsPage
       },
       {
         path: 'about',
@@ -35,7 +31,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/(home:home)',
+    redirectTo: '/tabs/(news:news)',
     pathMatch: 'full'
   }
 ];
@@ -44,4 +40,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
